66fc75d514c49e686287ff2223a4047a11cd79fb
Santhosh-004 <santhoshg0004@gmail.com>
2024-02-15 19:20:43 +0530

    using if conditions

---
+++ if statements/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+   int age;
+
+   std::cout << "Enter your age: ";
+   std::cin >> age;
+
+   if (age >= 18) {
+      std::cout << "You are an adult" << std::endl;
+   } else {
+      std::cout << "You are not an adult" << std::endl;
+   }
+
+   return 0;
+}
